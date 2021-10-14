c03e0427a98546f8f25fe8da3b7a68fa163cfe96
Arbalest163 <evil_xacker@mail.ru>
2021-10-14 14:33:36 +0400

    Добавьте файлы проекта.

---
+++ Anketa/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿
+namespace Anketa
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox_LastName = new System.Windows.Forms.TextBox();
+            this.textBox_FirstName = new System.Windows.Forms.TextBox();
+            this.textBox_Sex = new System.Windows.Forms.TextBox();
+            this.textBox_Patronymic = new System.Windows.Forms.TextBox();
+            this.textBox_Info = new System.Windows.Forms.TextBox();
+            this.textBox_MaritalStatus = new System.Windows.Forms.TextBox();
+            this.textBox_BirthDay = new System.Windows.Forms.TextBox();
+            this.button_Save = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(27, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 54);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Фамилия:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(27, 297);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 54);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Пол:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(27, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(212, 54);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Отчество:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(27, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 54);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Имя:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(27, 381);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(340, 54);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Дата рождения:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(27, 476);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(461, 54);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Семейное положение:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(27, 558);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(380, 54);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Доп информация:";
+            // 
+            // textBox_LastName
+            // 
+            this.textBox_LastName.Location = new System.Drawing.Point(500, 38);
+            this.textBox_LastName.Name = "textBox_LastName";
+            this.textBox_LastName.Size = new System.Drawing.Size(448, 47);
+            this.textBox_LastName.TabIndex = 7;
+            this.textBox_LastName.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox_FirstName
+            // 
+            this.textBox_FirstName.Location = new System.Drawing.Point(500, 127);
+            this.textBox_FirstName.Name = "textBox_FirstName";
+            this.textBox_FirstName.Size = new System.Drawing.Size(448, 47);
+            this.textBox_FirstName.TabIndex = 8;
+            this.textBox_FirstName.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox_Sex
+            // 
+            this.textBox_Sex.Location = new System.Drawing.Point(500, 304);
+            this.textBox_Sex.Name = "textBox_Sex";
+            this.textBox_Sex.Size = new System.Drawing.Size(448, 47);
+            this.textBox_Sex.TabIndex = 10;
+            this.textBox_Sex.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox_Patronymic
+            // 
+            this.textBox_Patronymic.Location = new System.Drawing.Point(500, 215);
+            this.textBox_Patronymic.Name = "textBox_Patronymic";
+            this.textBox_Patronymic.Size = new System.Drawing.Size(448, 47);
+            this.textBox_Patronymic.TabIndex = 9;
+            this.textBox_Patronymic.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox_Info
+            // 
+            this.textBox_Info.Location = new System.Drawing.Point(500, 565);
+            this.textBox_Info.Name = "textBox_Info";
+            this.textBox_Info.Size = new System.Drawing.Size(448, 47);
+            this.textBox_Info.TabIndex = 13;
+            this.textBox_Info.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox_MaritalStatus
+            // 
+            this.textBox_MaritalStatus.Location = new System.Drawing.Point(500, 476);
+            this.textBox_MaritalStatus.Name = "textBox_MaritalStatus";
+            this.textBox_MaritalStatus.Size = new System.Drawing.Size(448, 47);
+            this.textBox_MaritalStatus.TabIndex = 12;
+            this.textBox_MaritalStatus.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox_BirthDay
+            // 
+            this.textBox_BirthDay.Location = new System.Drawing.Point(500, 388);
+            this.textBox_BirthDay.Name = "textBox_BirthDay";
+            this.textBox_BirthDay.Size = new System.Drawing.Size(448, 47);
+            this.textBox_BirthDay.TabIndex = 11;
+            this.textBox_BirthDay.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // button_Save
+            // 
+            this.button_Save.Location = new System.Drawing.Point(239, 700);
+            this.button_Save.Name = "button_Save";
+            this.button_Save.Size = new System.Drawing.Size(490, 134);
+            this.button_Save.TabIndex = 14;
+            this.button_Save.Text = "Сохранить";
+            this.button_Save.UseVisualStyleBackColor = true;
+            this.button_Save.Click += new System.EventHandler(this.button_Save_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileName = "Новый файл.txt";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(995, 872);
+            this.Controls.Add(this.button_Save);
+            this.Controls.Add(this.textBox_Info);
+            this.Controls.Add(this.textBox_MaritalStatus);
+            this.Controls.Add(this.textBox_BirthDay);
+            this.Controls.Add(this.textBox_Sex);
+            this.Controls.Add(this.textBox_Patronymic);
+            this.Controls.Add(this.textBox_FirstName);
+            this.Controls.Add(this.textBox_LastName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox_LastName;
+        private System.Windows.Forms.TextBox textBox_FirstName;
+        private System.Windows.Forms.TextBox textBox_Sex;
+        private System.Windows.Forms.TextBox textBox_Patronymic;
+        private System.Windows.Forms.TextBox textBox_Info;
+        private System.Windows.Forms.TextBox textBox_MaritalStatus;
+        private System.Windows.Forms.TextBox textBox_BirthDay;
+        private System.Windows.Forms.Button button_Save;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Anketa/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Anketa
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Save_Click(object sender, EventArgs e)
+        {
+            var anketa = new Anketa();
+            if (saveAnketa(anketa))
+            {
+                saveFile(anketa);
+            }
+
+        }
+
+        private void saveFile(Anketa anketa)
+        {
+            try
+            {
+               
+                using SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+                saveFileDialog1.Filter = "txt files (*.txt)|*.txt";
+                saveFileDialog1.FilterIndex = 1;
+                saveFileDialog1.RestoreDirectory = true;
+
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    StreamWriter myStream = new StreamWriter(saveFileDialog1.FileName);
+
+                    myStream.WriteLine($"Фамилия: {anketa.LastName}\n" +
+                                       $"Имя: {anketa.FirstName}\n" +
+                                       $"Отчество: {anketa.Patronimyc}\n" +
+                                       $"Пол: {anketa.Sex}\n" +
+                                       $"Дата рождения: {anketa.BirthDay}\n" +
+                                       $"Семейное положение: {anketa.MaritalStatus}\n" +
+                                       $"Дополнительная информация: {anketa.Info}\n");
+
+                    myStream.Close();
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private bool saveAnketa(Anketa anketa)
+        {
+            try
+            {
+                if (textBox_LastName.Text == "" || textBox_FirstName.Text == "" ||
+                    textBox_Patronymic.Text == "" || textBox_Sex.Text == "" ||
+                    textBox_BirthDay.Text == "" || textBox_MaritalStatus.Text == "" ||
+                    textBox_Info.Text == "")
+                {
+                    throw new Exception();
+                }
+                else 
+                {
+                    anketa.LastName = textBox_LastName.Text;
+                    anketa.FirstName = textBox_FirstName.Text;
+                    anketa.Patronimyc = textBox_Patronymic.Text;
+                    anketa.Sex = textBox_Sex.Text;
+                    anketa.BirthDay = textBox_BirthDay.Text;
+                    anketa.MaritalStatus = textBox_MaritalStatus.Text;
+                    anketa.Info = textBox_Info.Text;
+                }
+
+            }
+            catch (Exception) {
+                MessageBox.Show("Анкета не заполнена");
+                return false;
+            }
+            return true;
+        }
+    }
+    public class Anketa
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Patronimyc { get; set; }
+        public string Sex { get; set; }
+        public string BirthDay { get; set; }
+        public string MaritalStatus { get; set; }
+        public string Info { get; set; }
+
+    }
+}
